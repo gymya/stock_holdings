a7fccbfe6c3ffd15dcf7b59fa98602a93a7199fb
Benny <benny88811@gmail.com>
2025-02-13 22:33:35 +0800

    改為資料庫

--- middleware/convertXLSX.js
+++ middleware/convertXLSX.js
@@ -1,14 +1,20 @@
 const convertXLSX = (jsonData) => {
-  const XLSX = require('xlsx');
-  const workbook = XLSX.utils.book_new();
+  try {
+    const XLSX = require('xlsx');
+    const workbook = XLSX.utils.book_new();
 
-  const ws = XLSX.utils.json_to_sheet(jsonData);
-  XLSX.utils.book_append_sheet(workbook, ws, 'Sheet1');
-  const excelBuffer = XLSX.write(workbook, {
-    type: 'buffer',
-    bookType: 'xlsx',
-  });
-  return excelBuffer;
+    const ws = XLSX.utils.json_to_sheet(jsonData);
+
+    XLSX.utils.book_append_sheet(workbook, ws, 'Sheet1');
+    const excelBuffer = XLSX.write(workbook, {
+      type: 'buffer',
+      bookType: 'xlsx',
+    });
+    return excelBuffer;
+  } catch (error) {
+    console.error('Error converting JSON to XLSX:', error);
+    throw error;
+  }
 };
 
 module.exports = convertXLSX;
--- routes/holdings.js
+++ routes/holdings.js
@@ -1,6 +1,48 @@
 var express = require('express');
 const axios = require('axios');
 const convertXLSX = require('@/middleware/convertXLSX');
+var Agenda = require('agenda');
+var mongoose = require('mongoose');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  ScanCommand,
+} = require('@aws-sdk/lib-dynamodb');
+
+// connect MongoDB
+
+const dynamoClient = new DynamoDBClient({ region: 'ap-northeast-1' });
+const docClient = DynamoDBDocumentClient.from(dynamoClient);
+const TABLE_NAME = 'StockData';
+
+// mongoose
+//   .connect('mongodb://127.0.0.1/my_database', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+const agenda = new Agenda({
+  db: {
+    address: 'mongodb://localhost:27017/agenda-db',
+    collection: 'agendaJobs',
+  },
+});
+
+agenda.on('ready', () => {
+  console.log('Agenda started');
+  // Schedule the job to run every day at midnight
+  agenda.every('0 0 * * *', 'fetch and store stock data');
+  // Start the agenda
+  agenda.start();
+});
+
+agenda.on('error', (err) => {
+  console.error('Agenda connection error:', err);
+});
 
 var router = express.Router();
 
@@ -61,49 +103,82 @@ const getVolKData = async (symbol) => {
   }
 };
 
-const getStockSymbol = async (req, res, next) => {
+const getStockSymbol = async () => {
   try {
     const { data } = await axios.get(
       'https://openapi.twse.com.tw/v1/opendata/t187ap03_L'
     );
     const symbolArray = data.map((stock) => stock.公司代號);
-
-    res.symbolArray = symbolArray;
-    next();
+    return symbolArray;
   } catch (error) {
     console.log(error);
-    res.status(500).send('Error');
   }
 };
 
-const getBrokerTrades = async (req, res, next) => {
-  res.VolKData = [];
-  // forEach is not working async, so we use for loop
-  for (let i = 0; i < res.symbolArray.length; i++) {
-    const VolKData = await getVolKData(res.symbolArray[i]);
-    res.VolKData.push(VolKData);
+agenda.define('fetch and store stock data', async (job) => {
+  console.log('Fetching and storing stock data...');
+
+  try {
+    const symbolList = await getStockSymbol();
+    const volKList = [];
+    for (let i = 0; i < symbolList.length; i++) {
+      const VolKData = await getVolKData(symbolList[i]);
+      volKList.push(VolKData);
+    }
+
+    // Save data to MongoDB
+    const command = new PutCommand({
+      TableName: TABLE_NAME,
+      Item: volKList,
+    });
+    await docClient.send(command);
+
+    // await StockData.insertMany(volKList);
+  } catch (error) {
+    console.log(error);
   }
-  next();
-};
+});
 
-const createXLSX = async (req, res, next) => {
-  res.excelBuffer = convertXLSX(res.VolKData);
-  next();
-};
+router.get('/holdings', async (req, res, next) => {
+  try {
+    const now = new Date();
+    const formattedDate = now.toISOString().slice(0, 10).replace(/-/g, '-');
 
-const holdingsMiddleware = [getStockSymbol, getBrokerTrades, createXLSX];
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=${
+        formattedDate + encodeURIComponent('主力資料')
+      }.xlsx`
+    );
 
-router.get('/holdings', holdingsMiddleware, (req, res, next) => {
-  const now = new Date();
-  const formattedDate = now.toISOString().slice(0, 10).replace(/-/g, '-');
+    // Fetch data from MongoDB
 
-  res.setHeader(
-    'Content-Disposition',
-    `attachment; filename=${
-      formattedDate + encodeURIComponent('主力資料')
-    }.xlsx`
-  );
-  res.send(res.excelBuffer);
+    const command = new ScanCommand({ TableName: TABLE_NAME });
+    const stockData = await docClient.send(command);
+
+    // const stockData = await StockData.find(
+    //   {},
+    //   {
+    //     symbol: 1,
+    //     totalOverbuyVolK: 1,
+    //     totalOversellVolK: 1,
+    //     tradeVolumeRate: 1,
+    //     totalDifferenceVolK1D: 1,
+    //     totalDifferenceVolK5D: 1,
+    //     totalDifferenceVolK10D: 1,
+    //     totalDifferenceVolK20D: 1,
+    //     _id: 0,
+    //   }
+    // );
+
+    // because stockData is a Mongoose document, we need to convert it to a plain object
+    const temp = stockData.map((stock) => stock.toObject());
+
+    // Convert data to XLSX
+    res.send(convertXLSX(temp));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
